7bcad5df03c9211e6e84524d1add0ea92958ac44
Waqas Ahmed <150505877+waqasahmedgh@users.noreply.github.com>
2024-02-21 16:26:08 +0500

    Add files via upload

---
+++ calculator.ts
@@ -0,0 +1,73 @@
+import inquirer from "inquirer";
+
+function main() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "num1",
+        message: "Enter the First Number:",
+        
+        validate(value:number) {
+          const valid = !isNaN(value);
+          return valid || "Please enter Number Only";
+        },
+        
+      },
+      {
+        type: "input",
+        name: "num2",
+        message: "Enter the Second Number:",
+        validate(value:number) {
+          const valid = !isNaN(value);
+          return valid || "Please enter Number Only";
+        },
+      },
+      {
+        type: "list",
+        name: "operation",
+        choices: ["+", "-", "*", "/"],
+        message: "Select Your Operation",
+      },
+    ])
+    .then((answers) => {
+      const { operation, num1, num2 } = answers;
+      if (operation == "+") {
+        sum(Number(num1), Number(num2));
+      } else if (operation == "-") {
+        subtract(Number(num1), Number(num2));
+      } else if (operation == "*") {
+        multiply(Number(num1), Number(num2));
+      } else if (operation == "/") {
+        division(Number(num1), Number(num2));
+      } else {
+        console.log("Invalid Operation");
+      }
+      // Use user feedback for... whatever!!
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+      } else {
+        // Something else went wrong
+      }
+    });
+}
+function sum(num1: number, num2: number) {
+  const result = num1 + num2;
+  console.log("Sum is", result);
+}
+function subtract(num1: number, num2: number) {
+  const result = num1 - num2;
+  console.log("Subtraction is", result);
+}
+function multiply(num1: number, num2: number) {
+  const result = num1 * num2;
+  console.log("Multiply is", result);
+}
+function division(num1: number, num2: number) {
+  const result = num1 / num2;
+  console.log("Division is", result);
+}
+
+main();
